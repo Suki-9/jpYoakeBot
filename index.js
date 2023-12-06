8d82f19a2f3a00d9a35a23e8f0507016873ff0b4
suki-9 <suki.music2004@gmail.com>
2023-12-06 18:17:29 +0900

    fix: "なんかもう全部治した"

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
+const color = { red:'\u001b[31m',reset:'\u001b[0m',green:'\u001b[32m',blue:'\u001b[34m',bold:'\x1b[1m'};
 const config = require('js-yaml').load(require('fs').readFileSync("./config.yaml", 'utf8'));
-const jobs = {
-  init: function () {
-    
-  },
-  getSunrise: require('node-cron').schedule('*/5 * * * * *', async () => {
-    const sunrise = new Date(await fetch('https://api.sunrise-sunset.org/json?lat=35.410p00&lag=139.69200&date=today&formatted=0&tzid=Asia/Tokyo'))
-      .then(r => r.ok ? r.json() : { err: '失敗しました。' })
-      .then(d => d.results.sunrise);
+console.log(`${color.bold}日本の夜明けBot (v0.1.0)${color.reset}\nDeveloper: Suki(${color.green}misskey.io${color.reset} https://misskey.io/@suki_music)\n\n[${color.blue}configs${color.reset}]\n instanceUrl: ${config.instanceUrl}\n token: ${config.token}\n`)
 
-    console.log('日本の夜明けを取得。');
-
-      this.post = require('node-cron').schedule(`${sunrise.getSeconds()} ${sunrise.getMinutes()} ${sunrise.getHours()} * *`, async () =>
-        fetch(`${config.instanceUrl}/api/notes/create`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            i: config.token,
-            text: "日本の夜明けぜよおおおぉぉぉぉ！",
-          }),
-        })
-      )
-  }),
-};
-
-jobs.init();
+require('node-cron').schedule('0 0 3 * * *', async () => {
+  const sunrise = new Date(await fetch('https://api.sunrise-sunset.org/json?lat=35.410000&lng=139.69200&date=today&formatted=0').then(r => r.json()).then(d => d.results.sunrise));
+  console.log(`[${color.green}log${color.reset}]: fetch sunrise time -> ${color.green}${sunrise}${color.reset}`)
+  setTimeout(() => fetch(`${config.instanceUrl}/api/notes/create`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      i: config.token,
+      text: "日本の夜明けぜよおおおぉぉぉぉ！",
+    }),
+  })
+    .then(r => r.ok ? console.log(`[${color.green}log${color.reset}]: Successful submission!`) : console.log(`[${color.red}Error!${color.reset}]: Posting failed!`)),
+  sunrise.getTime() - new Date().getTime());
+})
